e96a9fa88f80493c5e0ad483c0d11bef2b766ec7
Xante <sawenger@gmail.com>
2024-09-19 21:39:42 -0400

    Create localization.js

---
+++ localization.js
@@ -0,0 +1,44 @@
+const translations = {
+    'en': {
+        'pid': 'Player ID',
+        'plogin': 'Last login',
+        'pip': 'IP Address',
+        'pscore': 'Points',
+        'pvmode': 'Vacation Mode'
+    },
+    'de': {
+        'pid': 'Player ID',
+        'plogin': 'Letzter Login',
+        'pip': 'IP Adresse',
+        'pscore': 'Punkte',
+        'pvmode': 'Urlaubsmodus'
+    },
+    'fr': {
+        'pid': 'ID du joueur',
+        'plogin': 'Dernier login',
+        'pip': 'Adresse IP',
+        'pscore': 'Points',
+        'pvmode': 'Mode vacances'
+    },
+    'es': {
+        'pid': 'ID del jugador',
+        'plogin': 'Último login',
+        'pip': 'Dirección de IP',
+        'pscore': 'Puntos',
+        'pvmode': 'Modo de vacaciones'
+    },
+    'it': {
+        'pid': 'ID giocatore',
+        'plogin': 'Ultimo login',
+        'pip': 'Indirizzo IP',
+        'pscore': 'Punti',
+        'pvmode': 'Modalità vacanza'
+    },
+    'nl': {
+        'pid': 'Speler ID',
+        'plogin': 'Laatste login',
+        'pip': 'IP-adres',
+        'pscore': 'Punten',
+        'pvmode': 'Vakantiemodus'
+    }
+};
