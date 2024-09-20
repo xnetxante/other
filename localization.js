49feb4f0962ea9b1cafb7d232e64d6428cdf58f4
Xante <sawenger@gmail.com>
2024-09-19 21:53:42 -0400

    Update localization.js
    Added more translated stuff

--- localization.js
+++ localization.js
@@ -1,44 +1,81 @@
+// localization.js
+
 const translations = {
     'en': {
         'pid': 'Player ID',
         'plogin': 'Last login',
         'pip': 'IP Address',
         'pscore': 'Points',
-        'pvmode': 'Vacation Mode'
+        'pvmode': 'Vacation Mode',
+        'selectedLifeformsTxt': 'Selected',
+        'researchQueueTxt': 'Research in progress',
+        'fleetQueueTxt': 'Construction queue - Fleets',
+        'buildingQueueTxt': 'Construction queue - buildings'
     },
     'de': {
         'pid': 'Player ID',
         'plogin': 'Letzter Login',
         'pip': 'IP Adresse',
         'pscore': 'Punkte',
-        'pvmode': 'Urlaubsmodus'
+        'pvmode': 'Urlaubsmodus',
+        'selectedLifeformsTxt': 'Ausgewählt',
+        'researchQueueTxt': 'Laufende Forschung',
+        'fleetQueueTxt': 'Bauliste - Flotten',
+        'buildingQueueTxt': 'Bauliste - Gebäude'
     },
     'fr': {
         'pid': 'ID du joueur',
         'plogin': 'Dernier login',
         'pip': 'Adresse IP',
         'pscore': 'Points',
-        'pvmode': 'Mode vacances'
+        'pvmode': 'Mode vacances',
+        'selectedLifeformsTxt': 'Sélectionné',
+        'researchQueueTxt': 'Recherche en cours',
+        'fleetQueueTxt': 'Liste de construction - Flottes',
+        'buildingQueueTxt': 'Liste de construction - Bâtiments'
     },
     'es': {
         'pid': 'ID del jugador',
         'plogin': 'Último login',
         'pip': 'Dirección de IP',
         'pscore': 'Puntos',
-        'pvmode': 'Modo de vacaciones'
+        'pvmode': 'Modo de vacaciones',
+        'selectedLifeformsTxt': 'Selección',
+        'researchQueueTxt': 'Investigación en curso',
+        'fleetQueueTxt': 'Cola de construcción - Escuadrón',
+        'buildingQueueTxt': 'Cola de construcción - Edificios'
     },
     'it': {
         'pid': 'ID giocatore',
         'plogin': 'Ultimo login',
         'pip': 'Indirizzo IP',
         'pscore': 'Punti',
-        'pvmode': 'Modalità vacanza'
+        'pvmode': 'Modalità vacanza',
+        'selectedLifeformsTxt': 'Selezionata',
+        'researchQueueTxt': 'Ricerca in corso',
+        'fleetQueueTxt': 'Coda costruzione - Flotte',
+        'buildingQueueTxt': 'Coda costruzione - Strutture'
     },
     'nl': {
         'pid': 'Speler ID',
         'plogin': 'Laatste login',
         'pip': 'IP-adres',
         'pscore': 'Punten',
-        'pvmode': 'Vakantiemodus'
+        'pvmode': 'Vakantiemodus',
+        'selectedLifeformsTxt': 'Geselecteerd',
+        'researchQueueTxt': 'Onderzoek in uitvoering',
+        'fleetQueueTxt': 'Constructie wachtrij - Vloten',
+        'buildingQueueTxt': 'Constructie wachtrij - Gebouwen'
     }
 };
+
+// Function to get translations based on language
+function getTranslation(key, locale) {
+    if (translations[locale] && translations[locale][key]) {
+        return translations[locale][key];
+    } else if (translations['en'][key]) { // Fallback to English
+        return translations['en'][key];
+    } else {
+        return key; // Fallback to key if no translation found
+    }
+}
