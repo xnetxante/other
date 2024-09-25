68ce4db9138dcee9843240bb33eb87b1382896e3
Xante <sawenger@gmail.com>
2024-09-25 18:24:26 -0400

    Create tools.js

---
+++ tools.js
@@ -0,0 +1,21 @@
+/* Central User Script Tools
+@description        Tools For All Scripts 
+@version            Version 1.5
+@author             Xante
+
+/* Change Log */ {
+    /* NewDate (version) */ /* coder
+    - Changes
+    */
+
+    /* Sept 25, 2024 (1.0) */ /* Xante
+    - Created this Script
+    */
+
+
+}
+
+/* Clear Info Page */ function clearInfoBox() {
+    var infobox = document.querySelector('.infobox');
+    infobox.innerHTML = "";
+}
